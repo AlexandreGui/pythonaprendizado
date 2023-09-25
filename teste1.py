fcdd4e0875c76aa8d5edc11661f594494a5f4877
Alexandre Guimaraes <alexandre1994adm@gmail.com>
2023-09-25 16:43:52 -0300

    adicionado algoritimo que detecta se e par ou impa

--- teste1.py
+++ teste1.py
@@ -1,1 +1,10 @@
 print ('ola mundo')
+n1 = int (input ('Digite um número'))
+n2 = int ( input ('Digite outor número'))
+s1 = n1 + n2
+if (s1%2) == 0:
+    print ('esse numero e par')
+else:
+    print ('esse numero e impar')
+
+print (s1)
