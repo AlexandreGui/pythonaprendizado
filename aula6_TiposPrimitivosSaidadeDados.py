f5af67e176e815742afe6d5917c4d32cb2cb0402
Alexandre Guimaraes <alexandre1994adm@gmail.com>
2023-09-27 14:36:37 -0300

    add ala 6 e 7

---
+++ aula6_TiposPrimitivosSaidadeDados.py
@@ -0,0 +1,17 @@
+# nessa aula vou relembrar alguns principais usos da linguagem python do mundo um do curso em video
+# lembrando que eu ja sei tudo, só estou relembrando e documentando no git.
+''' aspas simples (mas funciona as duplas) 
+input significa entrada de dado armazenado em uma variavel.
+ex n1 = int (input('Digite um número'))
+   n2 = int (input('Digite outro número'))
+   s = n1 + n2
+   print ('A soma valor ', s)
+
+tipos primitivos
+int (10,2,5)
+float (4.5, 0.076)
+bool (True , False) lembrando que deve ter letra maiuscula no inicio.
+str  ( 'oi' , 'palavra' ) tem que estar entre aspas simples (mas vale aspas dupplas)
+
+
+ '''
---
+++ aula7_OperadoresAritimeticos.py
@@ -0,0 +1,32 @@
+# Nessa aula será utilizado operadores aritiméticos e suas preferencias
+''' 
+Operadores aritimeticos
+
++ adicao    5 + 2 == 7
+
+- subtracao   5 - 2 == 3
+
+* Multiplicacao   5 * 2 == 10 
+
+/ Divisao   5 / 2 == 2.5
+
+** potencia    5 ** 2 == 25
+
+// divisao inteira   5 // 2 == 2
+
+% resto da divisao  5 % 2 ==  1
+
+
+
+Ordem de precedencia
+
+1   ()  
+
+2  **
+
+3  * / // % (quem aparecer primeiro)
+
+3  +    - 
+
+
+ '''
