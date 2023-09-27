b5ac7c50e75a5ddf762f6f382cecc0bfacd54d23
Alexandre Guimaraes <alexandre1994adm@gmail.com>
2023-09-27 19:22:03 -0300

    aula 10 e 11

---
+++ aula10_Condicoes01.py
@@ -0,0 +1,37 @@
+# primeira aula sobre condicoes.
+'''
+Condicoes simples
+
+if carro.esquerda():
+    siga
+
+else:
+    esquerda
+
+lembrando que deve ser feito um tabulacao em python.
+
+tempo = int(input('Quantos anos tem seu carro? '))
+
+if tempo <=3:
+    print('Carro novo')
+else:
+    print('Carro Velho')
+
+print('Fim')
+
+condicao simplificada
+print('carro novo') if tempo <=3 else 'carro velho')
+print('fim')
+
+exemplo pratico :
+
+nome = str(input('Qual é o seu nome? '))
+if nome == 'Gustavo': 
+    print('Que lindo voce tem!')
+else:
+    print('Seu nome e tao normal')
+print('Bom dia,{:.1}!'.format(nome))
+{:.1} print de formatacao para 1 casa decimal.
+
+
+'''
---
+++ aula11_CoresnoTerminal.py
@@ -0,0 +1,38 @@
+# Nessa aula será para aprender sobre cores no terminal.
+
+'''
+Padrão ansi
+escape sequence
+\033[m (assim para abrir e fechar o codigo de cores)
+\033[(style),(textcolor),(backgroundcolo)m
+
+
+codigo para estilo:
+0 - none
+1 - bold (negrito)
+4 - Underline (sublinhado)
+7 - 7 negativo, inverte as cores do background e text color.
+
+codigo para cor de texto:
+
+30 - branco - padrao
+31 - vermelho
+32 - verde
+33 - Amarelo 
+34 - azul
+35 - lilas
+36 - ciano
+37 - cinza
+
+Codigo para background:
+40 - branco
+41 - vermelho
+42 - verde 
+43 - amarelo
+44 - azul
+45 - lilas
+46 - ciano
+47 - cinza
+
+
+'''
