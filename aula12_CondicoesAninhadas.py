3a3a5142cdaa1cbb206d04496257f54df33e22c8
Alexandre Guimaraes <alexandre1994adm@gmail.com>
2023-09-28 06:55:33 -0300

    aula 12 e 13

---
+++ aula12_CondicoesAninhadas.py
@@ -0,0 +1,33 @@
+
+# Nessa aula será para utilizar os comandos if...elif...else em programas python
+
+''' 
+if carro.esquerda():
+    print...
+elif carro.direita():
+    print...
+elif carro.re():
+    print...
+else:
+    print...
+
+(pode ter quantos elif necessarios, porem somente 1 else)
+    
+exemplo:
+
+nome = str(input('Qual é o seu nome? '))
+if nome == 'Gustavo''
+    print('Que nome bonito!' )
+elif nome == 'Pedro'or nome == 'Maria'or nome == 'Paulo':
+    print('Seu nome é bem popular no Brasil. ')
+elif nome in 'Ana Claudia Jessica Juliana':
+    print('Belo nome feminimo')
+else:
+    print('Seu nome é bem normal. ')
+print('Tenha um bom dia, {}! '.format(nome))
+
+* Ponto importante aqui, elif, if voce pode colocar 'OR' entre as formas, justamente para achar mais dentro daqueles modelos. o 'IN' tambem pode ser usado aqui
+O 'IN' funcionar para ele pegar alguam palavra que tenha ali dentro e aplicar no if, muito interessante para genero quando for utilizar.
+Else opcional.
+
+'''
---
+++ aula13_EstruturadeRepeticaoFor.py
@@ -0,0 +1,38 @@
+
+# Nessa aula será abordado a estrutura de repeticao for , muito bom para listas, tuplas e bibliotecas.
+
+''' 
+logica de um bonequinho andando em blocos que precisa pular e no segundo exemplo pega a moeda.
+
+for c in range (0,3):
+    passo
+    pula
+passo 
+pega
+
+for c in range (0,10,2):   (começa no zero, vai ate o 9, de 2 em 2)
+    se tive moeda:
+        pega
+    passo
+    pula
+
+    for c in range (0,6):
+    print('oi')
+print('fim')
+
+
+for c in range (6,0,-1):
+    print(c)
+
+    
+s = 0
+for c in range (0,4):
+    n = int (input('Digite um valor: '))
+    s += n 
+print('A soma vale: {}'.format(s))
+print('fim')
+
+usando nas listas e tublas e dicionario, usando o len deles voce consegue pegar o tamanho total.
+
+
+'''
